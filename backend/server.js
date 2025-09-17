1f548d75b484ad877dcd104fa58d300528d75508
yash-shende99 <yashshende9999@gmail.com>
2025-09-17 05:49:42 +0530

    feat: initial implementation of server.js

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+// import uploadOnCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+
+const app=express()
+const port=process.env.PORT || 4000
+
+connectDB()
+
+
+//middleware
+
+app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+//api endpoint
+
+app.use('/api/admin',adminRouter)
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
+
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING Great')
+})
+
+
+app.listen(port,()=>console.log('server running on port',port))
+
+
